ca4960aacc68a959f2f25e3d1654ebca8ba54196
Anzhela Nurieva <anzzhhela98@gmail.com>
2021-11-30 16:55:20 +0200

    Exam Prep

--- Entity-Framework-Core/Exam Preparation/C# DB Advanced Exam - 04 April 2021/TeisterMask/DataProcessor/Serializer.cs
+++ Entity-Framework-Core/Exam Preparation/C# DB Advanced Exam - 04 April 2021/TeisterMask/DataProcessor/Serializer.cs
@@ -1,10 +1,10 @@
 ﻿namespace TeisterMask.DataProcessor
 {
-    using System;
-    using System.Globalization;
-    using System.Linq;
     using Data;
+    using System;
+    using System.Linq;
     using Newtonsoft.Json;
+    using System.Globalization;
     using TeisterMask.Data.Models.Enums;
     using TeisterMask.DataProcessor.ExportDto;
     using Formatting = Newtonsoft.Json.Formatting;
@@ -13,7 +13,7 @@
     {
         public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
         {
-            var project = context
+            var projects = context
                 .Projects
                 .Where(p => p.Tasks.Any())
                 .ToList()
@@ -35,32 +35,22 @@
                 .ThenBy(p => p.ProjectName)
                 .ToList();
 
-            var xmlResult = XmlConverter.Serialize(project, "Projects");
-            ;
+            var xmlResult = XmlConverter.Serialize(projects, "Projects");
+
             return xmlResult;
         }
-
-        //Export all projects that have at least one task.
-        //For each project, export its name, tasks count, and if it has end(due) date which is represented like "Yes" and "No".
-        //For each task, export its name and label type.Order the tasks by name (ascending).
-        //Order the projects by tasks count (descending), then by name (ascending).
-        //NOTE: You may need to call.ToArray() function before the selection in order to detach entities from the database and avoid runtime errors (EF Core bug). 
-
 
         public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
         {
             var employees = context
                 .Employees
+                .ToList() //?
                 .Where(e => e.EmployeesTasks.Any(t => DateTime.Compare(t.Task.OpenDate,date) >= 0))
-                .OrderByDescending(e => e.EmployeesTasks.Count)
-                .ThenBy(e => e.Username)
-                .Take(10)
-                .ToList()
                 .Select(e => new
                 {
                     Username =  e.Username,
                     Tasks = e.EmployeesTasks
-                        .Where(t => DateTime.Compare(t.Task.OpenDate,date) >= 0)
+                        .Where(t => t.Task.OpenDate >= date)
                         .OrderByDescending(t => t.Task.DueDate)
                         .ThenBy(t => t.Task.Name)
                         .Select(t=> new
@@ -70,8 +60,12 @@
                             DueDate = t.Task.DueDate.ToString("d", CultureInfo.InvariantCulture),
                             LabelType = Enum.GetName(typeof(LabelType), t.Task.LabelType),
                             ExecutionType = Enum.GetName(typeof(ExecutionType), t.Task.ExecutionType),
-                        }).ToArray()
-                });
+                        }).ToList()
+                })
+                .OrderByDescending(e => e.Tasks.Count)
+                .ThenBy(e => e.Username)
+                .Take(10)
+                .ToList();
 
             var serializer = JsonConvert.SerializeObject(employees, Formatting.Indented);
 
